fcada133e9cc67100ef05ff1f743012c3291063c
Tonylex18 <anthonyagada2000@gmail.com>
2025-02-04 09:03:04 +0100

    Updated env file

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 const Add = () => {
 
-  const url = "http://localhost:4000";
+  const url = import.meta.env.VITE_API_URL;
 
   const [image, setImage] = useState(false)
   const [data, setData] = useState({
